deda40d156ea184a778eaed70606657d40d3ea8a
LazBean <75616163+LazBean@users.noreply.github.com>
2026-03-11 04:52:56 +0900

    refactor: remove unused get_rate method
    - No longer called anywhere; replaced by get_rates

--- lib/rate_api_client.rb
+++ lib/rate_api_client.rb
@@ -5,10 +5,6 @@ class RateApiClient
   headers 'token' => ENV.fetch('RATE_API_TOKEN', '04aa6f42aa03f220c2ae9a276cd68c62')
   default_timeout 10
 
-  def self.get_rate(period:, hotel:, room:)
-    get_rates([{ period: period, hotel: hotel, room: room }])
-  end
-
   def self.get_rates(attributes)
     self.post("/pricing", body: { attributes: attributes }.to_json)
   end
